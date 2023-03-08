6a90e907042ffc97b86ad8024a3ff58e2480a2cd
Janice R <rodriguezjaniece@gmail.com>
2023-03-08 14:33:43 -0500

    general styles in index.css for a tag and cta btn

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,8 +5,8 @@ import resume from '../../assets/resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={resume} download>Download Resume</a>
-        <a href='#contact'>Let's Talk</a>
+        <a href={resume} download className='btn'>Download Resume</a>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
