47b8f9efbc697270cf9bd99ff1d2659b04fb1e62
Janice R <rodriguezjaniece@gmail.com>
2023-03-09 19:56:55 -0500

    working on media queries for med and sm devices

---
+++ src/components/header/HeaderNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderNav = () => {
+  return (
+    <div className='header_nav'>
+        <nav>
+            <ul>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default HeaderNav
