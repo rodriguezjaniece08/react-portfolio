228357eb437b646837418b03b29d6028dfbe17f1
Janice R <rodriguezjaniece@gmail.com>
2023-03-10 12:36:16 -0500

    styled nav a hover and working on usestate for active section on fixed nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,12 +3,14 @@ import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {FaLaptopCode} from 'react-icons/fa'
 import {BiMessageSquareDetail, BiBook} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#home')
   return (
     <nav>
-      <a href='#home'><AiOutlineHome size='1.2rem'/></a>
-      <a href='#experience'><BiBook size='1.2rem'/></a>
+      <a href='#home' onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome size='1.2rem'/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook size='1.2rem'/></a>
       <a href='#projects'><FaLaptopCode size='1.2rem'/></a>
       <a href='#contact'><BiMessageSquareDetail size='1.2rem'/></a>
     </nav>
