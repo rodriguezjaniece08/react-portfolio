8b70e1a8a4997e8bee42dd8d4130c402547e38d4
Janice R <rodriguezjaniece@gmail.com>
2023-03-13 22:51:37 -0400

    changes made to experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,7 +4,21 @@ import './experience.css'
 const Experience = () => {
   return (
     <section id='experience'>
-      experience
+      <div className="grid-item item1">
+        <h1>Experience</h1>
+      </div>
+      <div className="grid-item item2">
+        <h5>/Problem solver</h5>
+        <p>Lorem ipsum dolor, sit amet consectetur 
+          adipisicing elit. 
+          Saepe magnam quidem 
+          praesentium aliquam deserunt, iste numquam 
+          animi culpa, nisi 
+          officiis repellendus? 
+          Incidunt facere praesentium dignissimos id 
+          dicta provident 
+          laboriosam amet.</p>
+      </div>
     </section>
   )
 }
