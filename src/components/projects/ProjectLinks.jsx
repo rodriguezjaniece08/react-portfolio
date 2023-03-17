ad1d83de47c20afeb35b74594e7eab923e6255f6
Janice R <rodriguezjaniece@gmail.com>
2023-03-16 21:54:41 -0400

    added projectlinks file to create onclickproject section to show

---
+++ src/components/projects/ProjectLinks.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const ProjectLinks = () => {
+  return (
+    <div className='projects'>
+        <div className='project-link front-end'>
+
+        </div>
+        <div className='project-link ux'>
+
+        </div>
+    </div>
+  )
+}
+
+export default ProjectLinks
