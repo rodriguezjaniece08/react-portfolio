73ada8542dbe65e0b48f7f12725877054ab229b0
Janice R <rodriguezjaniece@gmail.com>
2023-03-16 22:06:48 -0400

    created containers for projects and imported to projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './projects.css'
+import ProjectsLinks from './ProjectLinks'
 
 const Portfolio = () => {
   return (
     <section id='projects'>
      <div className='projects-header'>
         <h1>Design & Code</h1>
+        <ProjectsLinks />
      </div>
     </section>
   )
