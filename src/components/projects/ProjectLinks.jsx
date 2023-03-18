4162d0412b1ce1f467be812b90f5677703844b14
Janice R <rodriguezjaniece@gmail.com>
2023-03-17 22:08:03 -0400

    added text layout to container

--- src/components/projects/ProjectLinks.jsx
+++ src/components/projects/ProjectLinks.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import {FiTerminal} from 'react-icons/fi'
 
 
 const ProjectLinks = () => {
   return (
     <div className='projects'>
         <div className='project-link front-end'>
-
+            <FiTerminal size='1.9rem'/>
+            <h2>Front-End Developer.</h2>
         </div>
         <div className='project-link ux'>
-
+            <h2>UX Designer.</h2>
         </div>
     </div>
   )
