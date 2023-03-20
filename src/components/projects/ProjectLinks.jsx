c5a5f5efd6854598e90ee51b3cd6c14ad216b77d
Janice R <rodriguezjaniece@gmail.com>
2023-03-19 21:26:31 -0400

    changed projects-header position to add more room to viewpoint height

--- src/components/projects/ProjectLinks.jsx
+++ src/components/projects/ProjectLinks.jsx
@@ -7,14 +7,14 @@ const ProjectLinks = () => {
   return (
     <div className='projects'>
         <div className='project-link front-end'>
-            <FiTerminal size='1.9rem' className='frontend-icon' color=''/>
+            <FiTerminal size='1.9rem' className='frontend-icon' hoverColor='white'/>
             <h2>Front-End Developer.</h2>
-            <p></p>
+            <p>10+ projects</p>
         </div>
         <div className='project-link ux'>
             <TbTable size='1.9rem' className='ux-icon'/>
             <h2>UX Designer.</h2>
-            <p></p>
+            <p>4+ projects</p>
         </div>
     </div>
   )
