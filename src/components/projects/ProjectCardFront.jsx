7d355868a90c8735465f9809dd1593aa43ddea1f
Janice R <rodriguezjaniece@gmail.com>
2023-03-21 20:01:47 -0400

    file for front-end projects

---
+++ src/components/projects/ProjectCardFront.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectCardFront = () => {
+  return (
+    <div className='card-container'>
+      <div className = 'project-card'> 
+        Project 1 
+      </div>
+      <div className = 'project-card'>
+        project 2 
+      </div>
+      <div className = 'project-card'>
+        project 3
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCardFront
