dfcac6d84e570baf2827018ea4fc0702cf530433
Janice R <rodriguezjaniece@gmail.com>
2023-06-08 14:12:49 -0400

    added 2nd row of card layout

--- src/components/projects/ProjectCardFront.jsx
+++ src/components/projects/ProjectCardFront.jsx
@@ -8,10 +8,11 @@ import { CardBody, CardGroup, CardImg, CardText, CardTitle, Card } from 'reactst
 //project card component
 const ProjectCardFront = () => {
   return(
+    <>
    <CardGroup>
     <Card 
       body
-      className="text-center"
+      className="text-center mx-3"
       style={{
         width: '5rem',
         background: 'var(--color-bg-variant)'
@@ -24,7 +25,37 @@ const ProjectCardFront = () => {
         }}
         top
         width="100%"
-      />
+        />
+      <CardBody>
+        <CardTitle tag='h4'>
+          TaskIT
+        </CardTitle>
+        <CardText
+          className='mb-2'
+          tag='h6'
+          >
+            A task Management Application intended for individual or group use for managing any task built with love as a team.
+          </CardText>
+        <p><a href="https://rodriguezjaniece08.github.io/Final_Project/" target='blank'>Check it Out</a></p>
+      </CardBody>
+    </Card>
+
+    <Card 
+      body
+      className="text-center mx-3"
+      style={{
+        width: '3rem',
+        background: 'var(--color-bg-variant)'
+      }}> 
+      <CardImg
+        alt="Image of Taskit App"
+        src={projectImg1}
+        style={{
+          height:'23rem'
+        }}
+        top
+        width="100%"
+        />
       <CardBody>
         <CardTitle tag='h4'>
           TaskIT
@@ -35,38 +66,71 @@ const ProjectCardFront = () => {
           >
             A task Management Application intended for individual or group use for managing any task.
           </CardText>
-        <p><a href="https://rodriguezjaniece08.github.io/Final_Project/" target='blank'>Check it Out</a></p>
-      </CardBody>
-    </Card>
-
-    <Card 
-      body
-      className="text-center"
-      style={{
-        width: '3rem'
-      }}> 
-      <CardImg
-        alt="Image of Taskit App"
-        src={projectImg1}
-        style={{
-          height:'23rem'
-        }}
-        top
-        width="100%"
-      />
-      <CardBody>
-        <CardTitle tag='h2'>
-          TaskIT
-        </CardTitle>
-        <CardText
-          className='mb-2'
-          tag='h4'
-          >
-            A task Management Application intended for individual or group use for managing any task.
-          </CardText>
       </CardBody>
     </Card>
    </CardGroup>
+      <CardGroup>
+      <Card 
+        body
+        className="text-center mx-3"
+        style={{
+          width: '5rem',
+          background: 'var(--color-bg-variant)'
+        }}> 
+        <CardImg
+          alt="Image of Taskit App"
+          src={projectImg1}
+          style={{
+            height:'23rem'
+          }}
+          top
+          width="100%"
+        />
+        <CardBody>
+          <CardTitle tag='h4'>
+            TaskIT
+          </CardTitle>
+          <CardText
+            className='mb-2'
+            tag='h6'
+            >
+              A task Management Application intended for individual or group use for managing any task built with love as a team.
+            </CardText>
+          <p><a href="https://rodriguezjaniece08.github.io/Final_Project/" target='blank'>Check it Out</a></p>
+        </CardBody>
+      </Card>
+  
+      <Card 
+        body
+        className="text-center mx-3"
+        style={{
+          width: '3rem',
+          background: 'var(--color-bg-variant)'
+        }}> 
+        <CardImg
+          alt="Image of Taskit App"
+          src={projectImg1}
+          style={{
+            height:'23rem'
+          }}
+          top
+          width="100%"
+          />
+        <CardBody>
+          <CardTitle tag='h4'>
+            TaskIT
+          </CardTitle>
+          <CardText
+            className='mb-2'
+            tag='h6'
+            >
+              A task Management Application intended for individual or group use for managing any task.
+            </CardText>
+        </CardBody>
+      </Card>
+     </CardGroup>
+              </>
+   
   )
 }
 
