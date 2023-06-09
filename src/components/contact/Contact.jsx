ed1f1d9093cdfe812c9b9c99d04095a6fcb46ca2
Janice R <rodriguezjaniece@gmail.com>
2023-06-09 16:08:56 -0400

    customizing form layout for contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './contact.css'
 import {FaArrowRight} from 'react-icons/fa'
+import Form from './Form';
 
 const Contact = () => {
   return (
     <section id='contact'>
-      <div className='contact-grid'>
+      <div className='contact-grid grid1'>
       <h1>Let's <span className='connect-arrow'><FaArrowRight/></span><br/> Connect.</h1>
+      </div>
+
+      <div className='contact-grid grid2'>
+        <Form/>
       </div>
     
     
