50e1b6da2018bf71e18bd0e1397d59816d30d589
Janice R <rodriguezjaniece@gmail.com>
2023-06-10 10:45:15 -0400

    added comments to form for better understanding

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -2,28 +2,35 @@ import React, { useRef } from 'react';
 import {FaArrowRight} from 'react-icons/fa'
 import emailjs from '@emailjs/browser';
 
+
+// form connected to email.js to handle user submission for contact session with no backend configuration.
+
  const Form = () => {
-    const form = useRef();
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    //function handles email submission through email.js
     emailjs.sendForm('service_ge7u44r', 'template_3iubivb', form.current, 't9zV-eFaLg-5QreOx')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
-      e.target.reset()
+      //resets the form after submission
+      e.target.reset();
   };
+
   return (
     <form ref={form} onSubmit={sendEmail}>
     <label>Name</label>
-      <input id='name' type='text' required />
+      <input id='name' name='user_name' type='text' required />
     
     <label for='email id='email-label>Email
-      <input type="email" name="email" required />
+      <input type="email" name="user_email" required />
     </label>
+
     <label>Message
     <textarea name="message" id='message' rows='10' cols='50' />
     </label>
