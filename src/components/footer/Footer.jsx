db3e43896f9c37527206694b07c43df57035bac9
Janice R <rodriguezjaniece@gmail.com>
2023-06-11 13:43:32 -0400

    added transform/translate to footer icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin, BsGithub, BsWordpress} from 'react-icons/bs'
 
 
 const Footer = () => {
+  
   return (
     <section id='footer'>
-      footer
+      <div className="footer_socials">
+       
+        <a href='https://github.com/rodriguezjaniece08' target='_blank' className='m-5'><BsGithub className='tags' size='2.2em' /></a>
+        <a href='https://www.linkedin.com/in/janice-rodriguez' target='_blank' className='m-5 '><BsLinkedin className='tags' size='2.2em' /></a>
+        <a href='https://rodriguezjaniece.wordpress.com/' target='_blank' className='m-5'><BsWordpress className='tags' size='2.2em' /></a>
+     </div>
+        <div className='copyright'>© Janice Rodriguez 2023</div>
+
+       
     </section>
   )
 }
