468176a263b0ec6863a07bd3568ea5d4391f26ff
Janice R <rodriguezjaniece@gmail.com>
2023-06-14 19:11:28 -0400

    included comments for better readability

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,6 +4,7 @@ import {BsLinkedin, BsGithub, BsWordpress} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
+      {/* social links to my accounts */}
         <a href='https://github.com/rodriguezjaniece08' target='_blank' rel='noreferrer'><BsGithub className='header-tags'size='1.5em' /></a>
         <a href='https://www.linkedin.com/in/janice-rodriguez' target='_blank' rel='noreferrer'><BsLinkedin className='header-tags' size='1.5em' /></a>
         <a href='https://rodriguezjaniece.wordpress.com/' target='_blank' rel='noreferrer'><BsWordpress className='header-tags' size='1.5em' /></a>
